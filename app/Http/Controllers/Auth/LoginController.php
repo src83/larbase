c33f0eebd37804701939a071b21c1999f47c0f6c
Roman Staroseltsev <r.staroseltsev@gmail.com>
2026-07-16 22:07:41 +0300

    fix(project): add return type to LoginController::logout()

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -65,7 +65,7 @@ class LoginController extends Controller
         return null;
     }
 
-    public function logout(Request $request)
+    public function logout(Request $request): JsonResponse|RedirectResponse
     {
         $this->guard()->logout();
 
